782d6890c67e2164cc4ba173a68964382a17b7eb
janniknu <jannik.nuesslein@gmx.de>
2024-10-14 23:14:37 +0200

    first settings

--- collection_service/models.py
+++ collection_service/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+class Collection(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    recipes = models.ManyToManyField(Recipe)
+    labels = models.JSONField(default=list)
--- collection_service/views.py
+++ collection_service/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Collection, Recipe, User
 
 # Create your views here.
+
+def get_collection(request, id):
+    collection = get_object_or_404(Collection, id=id)
+    return JsonResponse(collection_to_dict(collection))
