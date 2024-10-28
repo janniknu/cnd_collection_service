a97b7804f9a44fcd364c36204590b2a21e95b8aa
janniknu <jannik.nuesslein@gmx.de>
2024-10-28 20:13:46 +0100

    simlified rabbitmq service

--- collection_service/rabbitmq_service.py
+++ collection_service/rabbitmq_service.py
@@ -5,29 +5,20 @@ connection = pika.BlockingConnection(params)
 exchange = 'collection_service_exchange'
 channel = connection.channel()
 
+#Nur zum Debuggen
 channel.queue_declare(queue='collection.created')
 channel.queue_declare(queue='collection.updated')
 channel.queue_declare(queue='collection.deleted')
+
 channel.exchange_declare(exchange=exchange, exchange_type='direct')
 
+#Nur zum Debuggen
 channel.queue_bind(exchange=exchange,queue='collection.created',routing_key='collection.created')
 channel.queue_bind(exchange=exchange,queue='collection.updated',routing_key='collection.updated')
 channel.queue_bind(exchange=exchange,queue='collection.deleted',routing_key='collection.deleted')
 
-#connection.close()
-
 def publishEvent(method, body):
     #properties = pika.BasicProperties(method)
-    print(method)
-    print(body)
-    if method == 'collection.created':
-        channel.basic_publish(exchange=exchange, routing_key='collection.created', body=json.dumps(body))
-    elif method == 'collection.updated':
-        channel.basic_publish(exchange=exchange, routing_key='collection.updated', body=json.dumps(body))
-    elif method == 'collection.deleted':
-        channel.basic_publish(exchange=exchange, routing_key='collection.deleted', body=json.dumps(body))
-        print("published")
-    else:
-        print("Invalid method")
+        channel.basic_publish(exchange=exchange, routing_key=method, body=json.dumps(body))
 
     #channel.basic_publish(exchange=exchange, routing_key='hello', body=json.dumps(body + method), properties=properties) 
